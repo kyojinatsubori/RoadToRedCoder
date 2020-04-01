06ed22f4b949113b144eccbc88892369d5baea71
Snishiki <shunj1224@gmail.com>
2020-04-01 21:10:12 +0900

    4/1

--- Snishiki/AtCoder Beginner Contest/AtCoder Beginner Contest 158/C.py
+++ Snishiki/AtCoder Beginner Contest/AtCoder Beginner Contest 158/C.py
@@ -1,9 +1,9 @@
 import math
 A,B = map(int, input().split())
 answerA = math.ceil(A/0.08)
-answerAA = math.floor((A+1)/0.08)
+answerAA = math.ceil((A+1)/0.08 -1)
 answerB = math.ceil(B/0.1)
-answerBB = math.floor((B+1)/0.1)
+answerBB = math.ceil((B+1)/0.1 -1)
 if answerA == answerAA:
     if answerA >= answerB and answerA <= answerBB:
         print(answerA)
