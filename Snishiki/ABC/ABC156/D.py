02e4b94e18fa6133d120780f391223c0a36d8bb7
Snishiki <shunj1224@gmail.com>
2020-04-17 20:48:24 +0900

    4/18

---
+++ Snishiki/ABC/ABC156/D.py
@@ -0,0 +1,14 @@
+n,a,b = map(int,input().split())
+ans = 0
+
+
+for i in range(1 << n):
+    for j in range(n):
+        count = 0
+        if (i >> j) & 1 ==1:
+            count += 1
+    if count != a and count != b:
+        ans += 1
+print((ans-1)%(10**9+7))
+
+# 全部０引く
---
+++ tips/最大公約数と素因数分解.py
@@ -0,0 +1,29 @@
+# ABC142
+import fractions
+A,B = map(int,input().split())
+GCD = fractions.gcd(A,B)
+if GCD == 1:
+    print(1)
+    exit()
+    
+def factorization(n):
+    arr = []
+    temp = n
+    for i in range(2, int(-(-n**0.5//1))+1):
+        if temp%i==0:
+            cnt=0
+            while temp%i==0:
+                cnt+=1
+                temp //= i
+            arr.append([i, cnt])
+
+    if temp!=1:
+        arr.append([temp, 1])
+
+    if arr==[]:
+        arr.append([n, 1])
+
+    return arr
+
+Bunkai = factorization(GCD) 
+print(len(Bunkai) + 1)
