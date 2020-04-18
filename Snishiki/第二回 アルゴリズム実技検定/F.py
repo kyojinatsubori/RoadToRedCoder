3c8111a61af65221a4a11b3d69cbabec4b4ba0bc
Snishiki <shunj1224@gmail.com>
2020-04-18 21:21:33 +0900

    4/18

--- Snishiki/第二回 アルゴリズム実技検定/F.py
+++ Snishiki/第二回 アルゴリズム実技検定/F.py
@@ -7,10 +7,16 @@ sum=0
 lab=[]
 for k in range(1,n+1):
     #abの中でAiがkのものを足す
-    pin=[i for i in ab if ab[i][0]==k]
-    lab.extend(pin)
+    if len(ab) != 0:
+        while ab[0][0]<=k:
+            lab.append(ab[0][1])
+            ab.pop(0)
+            if len(ab) == 0:
+                break
     #その中でBiが最大の物を選択し、sumに足す
     max_lab=max(lab)
-
-    #abから選んだものを取り除く
+    sum += max_lab
+    #labから選んだものを取り除く
+    lab.pop(lab.index(max_lab))
     #sumを出力する
+    print(sum)
