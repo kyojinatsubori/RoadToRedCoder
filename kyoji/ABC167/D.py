5a3d8a1be3ecd6c75ee5be74c96028a23e6122a7
kyojinatsubori <natsubori777@gmail.com>
2020-05-10 22:34:55 +0900

    a

--- kyoji/ABC167/D.py
+++ kyoji/ABC167/D.py
@@ -1,35 +1,15 @@
-import math
-def binary_search(list,target):
-
-	result = -1
-	left = 0
-	right = len(list) - 1
-
-	while left <= right:
-		center = int((left + right)/2)
-		if list[center] == target:
-			result = center
-			break
-		elif list[center] < target:
-			left = center + 1
-		elif list[center] > target:
-			right = center - 1
-
-	if result == -1:
-		return False
-	else:
-		return True
-
 n,k=map(int,input().split())
 a=list(map(int,input().split()))
 i=0
 pasta=[1]
+memo=[0]*n
 while True:
-    if binary_search(pasta,a[i]):
+    if memo[a[i]-1]==1:
         roopy=pasta.index(a[i])
         roop=pasta[roopy:]
         break
     else:
+        memo[a[i]-1]+=1
         pasta.append(a[i])
         i=a[i]-1
 if k>len(pasta):
